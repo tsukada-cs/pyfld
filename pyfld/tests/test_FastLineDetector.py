913d52382845e6a61df31d706196960c8d2cbdd0
tsukada-cs <tsukada.cs@gmail.com>
2020-10-22 18:59:35 +0900

    add test_get_chained_points_Exception

--- pyfld/tests/test_FastLineDetector.py
+++ pyfld/tests/test_FastLineDetector.py
@@ -17,27 +17,27 @@ class TestFastLineDetector(unittest.TestCase):
         with self.assertRaises(ValueError):
             fld = FastLineDetector(canny_th2=-1)
 
-    def test_FLD_const_color(self, *mocks):
+    def test_FLD_const_color(self):
         fld = FastLineDetector()
         img = np.ones([30,30]).astype(np.uint8)
         segments = fld.detect(img)
         self.assertEqual(len(segments), 0)
     
-    def test_FLD_line(self, *mocks):
+    def test_FLD_line(self):
         fld = FastLineDetector()
         img = np.zeros([30,30]).astype(np.uint8)
         img[5:-5, 20] = 255
         segments = fld.detect(img)
         self.assertEqual(len(segments), 2) # 2 because of Gibbs effect
 
-    def test_FLD_edge_line(self, *mocks):
+    def test_FLD_edge_line(self):
         fld = FastLineDetector(canny_aperture_size=0)
         img = np.zeros([30,30]).astype(np.uint8)
         img[5:-5, 20] = 255
         segments = fld.detect(img)
         self.assertEqual(len(segments), 1)
     
-    def test_FLD_merge_lines(self, *mocks):
+    def test_FLD_merge_lines(self):
         fld = FastLineDetector(length_threshold=2, canny_aperture_size=0, do_merge=True)
         img = np.zeros([30,30]).astype(np.uint8)
         img[5:13, 15] = 255
@@ -58,7 +58,7 @@ class TestFastLineDetector(unittest.TestCase):
         self.assertEqual(point_chain[5], [[2, 3]])
         self.assertEqual(point_chain[6], [[0, 4]])
         
-    def test_get_chained_points_1(self, *mocks):
+    def test_get_chained_points_1(self):
         fld = FastLineDetector()
         img = np.zeros([5,5])
         img[:,2] = 1
@@ -67,7 +67,7 @@ class TestFastLineDetector(unittest.TestCase):
         self.assertEqual(points, ans_points)
         self.assertTrue(np.all(img == np.zeros_like(img)))
 
-    def test_get_chained_points_2(self, *mocks):
+    def test_get_chained_points_2(self):
         fld = FastLineDetector()
         img = np.array([
             [0,0,1,0,0],
@@ -81,7 +81,7 @@ class TestFastLineDetector(unittest.TestCase):
         self.assertEqual(points, ans_points)
         self.assertTrue(np.all(img == np.zeros_like(img)))
 
-    def test_get_chained_points_3(self, *mocks):
+    def test_get_chained_points_3(self):
         fld = FastLineDetector()
         img = np.array([
             [0,0,1,0,0],
@@ -102,7 +102,7 @@ class TestFastLineDetector(unittest.TestCase):
         self.assertEqual(points, ans_points)
         self.assertTrue(np.all(img == ans_img))
 
-    def test_get_chained_points_4(self, *mocks):
+    def test_get_chained_points_4(self):
         fld = FastLineDetector()
         img = np.eye(5)
         img += np.fliplr(np.eye(5))
@@ -118,7 +118,7 @@ class TestFastLineDetector(unittest.TestCase):
         self.assertEqual(points, ans_points)
         self.assertTrue(np.all(img == ans_img))
 
-    def test_get_chained_points_5(self, *mocks):
+    def test_get_chained_points_5(self):
         fld = FastLineDetector()
         img = np.array([
             [1,0,0,0,0,0],
@@ -133,8 +133,13 @@ class TestFastLineDetector(unittest.TestCase):
         points, img = fld.get_chained_points(img, Point(0,0))
         self.assertEqual(points, ans_points)
         self.assertTrue(np.all(img == np.zeros_like(img)))
+    
+    def test_get_chained_points_Exception(self):
+        fld = FastLineDetector()
+        with self.assertRaises(ValueError):
+            fld.get_chained_points(np.zeros([5,5]), Point(0,0))
 
-    def test_extract_segments_1(self, *mocks):
+    def test_extract_segments_1(self):
         fld = FastLineDetector(length_threshold=1, distance_threshold=0)
         points = [Point(0,0), Point(0,1), Point(1,2), Point(1,3),
                   Point(2,4), Point(3,5), Point(4,5), Point(5,5)]
@@ -142,7 +147,7 @@ class TestFastLineDetector(unittest.TestCase):
         segments = fld.extract_segments(points)
         self.assertTrue(np.all(np.round(segments) == np.array(segs)))
 
-    def test_extract_segments_2(self, *mocks):
+    def test_extract_segments_2(self):
         fld = FastLineDetector(length_threshold=2, distance_threshold=0)
         points = [Point(0,0), Point(0,1), Point(1,2), Point(1,3),
                   Point(2,4), Point(3,5), Point(4,5), Point(5,5)]
@@ -150,56 +155,56 @@ class TestFastLineDetector(unittest.TestCase):
         segments = fld.extract_segments(points)
         self.assertTrue(np.all(np.round(segments) == np.array(segs)))
 
-    def test_extract_segments_3(self, *mocks):
+    def test_extract_segments_3(self):
         fld = FastLineDetector(length_threshold=10, distance_threshold=0)
         points = [Point(0,0), Point(0,1), Point(1,2), Point(1,3),
                   Point(2,4), Point(3,5), Point(4,5), Point(5,5)]
         segments = fld.extract_segments(points)
         self.assertEqual(segments, [])
 
-    def test_dist_point_line_1(self, *mocks):
+    def test_dist_point_line_1(self):
         p1 = Point(0,0)
         p2 = Point(0,4)
         p3 = Point(2,2)
         dist = FastLineDetector.dist_point_line(p1, p2, p3)
         self.assertEqual(dist, 2)
 
-    def test_dist_point_line_2(self, *mocks):
+    def test_dist_point_line_2(self):
         p1 = Point(0,0)
         p2 = Point(0,0.1)
         p3 = Point(2,2)
         dist = FastLineDetector.dist_point_line(p1, p2, p3)
         self.assertEqual(dist, 2)
 
-    def test_incidet_point_1(self, *mocks):
+    def test_incidet_point_1(self):
         p1 = Point(0,0)
         p2 = Point(4,4)
         p3 = Point(0,4)
         pt = FastLineDetector.get_incident_point(p1, p2, p3)
         self.assertEqual(pt, Point(2,2))
         
-    def test_incidet_point_2(self, *mocks):
+    def test_incidet_point_2(self):
         p1 = Point(0,0)
         p2 = Point(0.1,0.1)
         p3 = Point(0,4)
         pt = FastLineDetector.get_incident_point(p1, p2, p3)
         self.assertEqual(pt, Point(2,2))
     
-    def test_merge_segments_1(self, *mocks):
+    def test_merge_segments_1(self):
         seg1 = Segment(0,0,3,0)
         seg2 = Segment(5,0,10,0)
         fld = FastLineDetector()
         seg_merged = fld.merge_segments(seg1, seg2)
         self.assertEqual(seg_merged, Segment(0,0,10,0))
 
-    def test_merge_segments_2(self, *mocks):
+    def test_merge_segments_2(self):
         seg1 = Segment(0,0,3,3)
         seg2 = Segment(5,5,10,10)
         fld = FastLineDetector()
         seg_merged = fld.merge_segments(seg1, seg2)
         self.assertEqual(seg_merged, Segment(0,0,10,10))
 
-    def test_merge_segments_3(self, *mocks):
+    def test_merge_segments_3(self):
         seg1 = Segment(0,0,10,10)
         seg2 = Segment(40,40,50,50)
         fld = FastLineDetector()
