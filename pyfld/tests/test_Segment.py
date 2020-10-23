28e1a9d4ba99f86d2f5d2bcf9247dfd352f2faa5
tsukada-cs <tsukada.cs@gmail.com>
2020-10-23 22:37:58 +0900

    fixed test_Segment.py

--- pyfld/tests/test_Segment.py
+++ pyfld/tests/test_Segment.py
@@ -6,12 +6,14 @@ class TestSegment(unittest.TestCase):
     """Test the Segment class."""
     def test_property(self):
         x1 = 0
-        x2 = 1
-        y1 = 2
-        y2 = 3
+        y1 = 0
+        x2 = 10
+        y2 = 0
         seg = Segment(x1, y1, x2, y2)
         self.assertEqual(seg.p1, Point(x1, y1))
         self.assertEqual(seg.p2, Point(x2, y2))
+        self.assertEqual(seg.length, 10)
+        self.assertEqual(seg.angle, 0)
 
     def test_swap_x(self):
         x1 = 0
