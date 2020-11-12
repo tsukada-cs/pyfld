417d4e9747d19d57b9eedf58df7d191c601ff10f
tsukada-cs <tsukada.cs@gmail.com>
2020-11-12 16:37:41 +0900

    v0.2.11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extensions = []
 
 setup(
     name="pyfld",
-    version="0.2.10",
+    version="0.2.11",
     url="https://github.com/tsukada-cs/pyfld",
     author="Taiga Tsukada",
     author_email="tsukada.cs@gmail.com",
